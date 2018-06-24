3095e26d5c6e381faaa3d6be725cbf2309316bea
Fábio Paiva <fabio@paiva.info>
2018-06-24 23:01:14 +0200

    Connect component to dispatch redux action to update all todo items status

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -5,7 +5,9 @@ import { withStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import List from '@material-ui/core/List'
+import Checkbox from '@material-ui/core/Checkbox'
 import TodoItem from '../TodoItem'
+import { setAllTodosStatus } from '../../actions/TodoActions'
 import type { Todo } from '../../types/todo'
 
 const styles = theme => ({
@@ -22,16 +24,25 @@ const styles = theme => ({
 type Props = {
   classes: Object,
   items: Array<Todo>,
+  setAllTodosStatus: (status: boolean) => void,
 }
 
-const TodoList = ({ classes, items }: Props) => {
+const TodoList = ({ classes, items, setAllTodosStatus: fnSetAllTodosStatus }: Props) => {
   if (!items.length) return null
+
+  const isAllItemsChecked = items.filter(item => item.completed).length === items.length
 
   return (
     <Grid container className={classes.root}>
       <Grid item xs={12}>
         <Grid container justify="center">
           <Paper className={classes.wrapper} elevation={0}>
+            <Grid container justify="flex-end">
+              <Checkbox
+                checked={isAllItemsChecked}
+                onChange={() => fnSetAllTodosStatus(!isAllItemsChecked)}
+              />
+            </Grid>
             <List component="nav">
               {items.map(item => <TodoItem key={item.id} item={item} />)}
             </List>
@@ -46,4 +57,4 @@ const mapStateToProps = state => ({
   items: state.todos.items,
 })
 
-export default withStyles(styles)(connect(mapStateToProps, { })(TodoList))
+export default withStyles(styles)(connect(mapStateToProps, { setAllTodosStatus })(TodoList))
