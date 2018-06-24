0edb1beb3406cd71da38e35e332802985ac42100
Fábio Paiva <fabio@paiva.info>
2018-06-24 21:53:30 +0200

    Allows to update a Todo item

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -7,6 +7,7 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import Checkbox from '@material-ui/core/Checkbox'
 import Icon from '@material-ui/core/Icon'
 import IconButton from '@material-ui/core/IconButton'
+import TodoInput from '../TodoInput'
 import { toggleTodo, removeTodo } from '../../actions/TodoActions'
 
 import type { Todo } from '../../types/todo'
@@ -17,7 +18,19 @@ type Props = {
   removeTodo: (id: number) => void,
 }
 
-class TodoItem extends React.Component<Props> {
+type State = {
+  isEditing: boolean,
+}
+
+class TodoItem extends React.Component<Props, State> {
+  state = {
+    isEditing: false,
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('click', this.handleClickOutside)
+  }
+
   handleToggle = () => {
     const { toggleTodo: fnToggle, item } = this.props
     fnToggle(item.id)
@@ -28,21 +41,58 @@ class TodoItem extends React.Component<Props> {
     fnRemove(item.id)
   }
 
+  handleItemClick = () => {
+    this.setState(
+      { isEditing: true },
+      () => setTimeout(() => window.addEventListener('click', this.handleClickOutside), 0),
+    )
+  }
+
+  handleClickOutside = (event) => {
+    if (event.target !== this.inputRefEditing) {
+      this.setState({ isEditing: false })
+      window.removeEventListener('click', this.handleClickOutside)
+    }
+  }
+
+  handleInputRefEditing = (ref: ?HTMLInputElement) => {
+    if (ref) {
+      this.inputRefEditing = ref
+      ref.focus()
+    }
+  }
+
+  handleFinishEditing = () => {
+    this.setState({ isEditing: false })
+    window.removeEventListener('click', this.handleClickOutside)
+  }
+
+  inputRefEditing: ?HTMLInputElement
+
   render() {
     const { item } = this.props
-    return (
-      <ListItem dense button>
-        <ListItemText primary={item.text} />
-        <ListItemSecondaryAction>
-          <Checkbox checked={item.completed} onChange={this.handleToggle} />
-          <IconButton aria-label="Delete" onClick={this.handleRemove}>
-            <Icon>
-              delete
-            </Icon>
-          </IconButton>
-        </ListItemSecondaryAction>
-      </ListItem>
+    const { isEditing } = this.state
+
+    return isEditing ? (
+      <TodoInput
+        item={item}
+        onUpdate={this.handleFinishEditing}
+        inputRef={this.handleInputRefEditing}
+      />
     )
+      : (
+        <ListItem dense button onClick={this.handleItemClick}>
+          <ListItemText primary={item.text} />
+          <ListItemSecondaryAction>
+            <Checkbox checked={item.completed} onChange={this.handleToggle} />
+            <IconButton aria-label="Delete" onClick={this.handleRemove}>
+              <Icon>
+                delete
+              </Icon>
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+      )
   }
 }
 
