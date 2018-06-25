970ca86f91903743ae250a59810976163994c61e
Fábio Paiva <fabio@paiva.info>
2018-06-25 16:50:37 +0200

    Firebase database rules Firebase project settings Save on Firebase a new todo Listen firebase DB

--- src/actions/TodoActions.js
+++ src/actions/TodoActions.js
@@ -1,10 +1,30 @@
 // @flow
 import types from '../constants/ActionTypes'
+import firebase from '../firebase'
+import type { User } from '../types/user'
 
-export const addTodo = (text: string) => ({
-  type: types.TODO_ADD,
-  text,
-})
+const database = firebase.database()
+
+export const addTodo = (text: string) => (dispatch: Function, getState: Function) => {
+  const { user }: { user: User } = getState()
+  const newItem = database.ref(`/todo/${user.profile.uid}`).push()
+  newItem.set({ text, uid: user.profile.uid })
+}
+
+export const listenFirebaseDBRef = () => (dispatch: Function, getState: Function) => {
+  const { user }: { user: User } = getState()
+  const ref = database.ref(`/todo/${user.profile.uid}`)
+  ref.on('value', (snapshot) => {
+    const val = snapshot.val()
+    if (val) {
+      dispatch({
+        type: types.TODO_SYNC,
+        items: snapshot.val(),
+      })
+    }
+  })
+  return { remove: () => ref.off('value') }
+}
 
 export const removeTodo = (id: number) => ({
   type: types.TODO_REMOVE,
